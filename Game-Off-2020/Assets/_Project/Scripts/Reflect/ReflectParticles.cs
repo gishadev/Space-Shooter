04c8ffba9b0b2d67ba3a2698b216633d48d1c685
GishaTheDeveloper <spaceshhz@gmail.com>
2020-11-21 21:53:30 +0200

    Add ReflectParticles.cs

---
+++ Game-Off-2020/Assets/_Project/Scripts/Reflect/ReflectParticles.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace SpaceGame.ReflectMovement
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class ReflectParticles : MonoBehaviour
+    {
+        ParticleSystem _pSystem;
+        ParticleSystem.Particle[] _particles;
+
+        Transform _transform;
+
+        private void Awake()
+        {
+            _pSystem = GetComponent<ParticleSystem>();
+            _particles = new ParticleSystem.Particle[_pSystem.main.maxParticles];
+
+            _transform = transform;
+        }
+
+        private void LateUpdate()
+        {
+            int nParticles = _pSystem.GetParticles(_particles);
+
+            for (int i = 0; i < nParticles; i++)
+            {
+                Vector3 worldPos = _transform.TransformPoint(_particles[i].position);
+
+                if (!ReflectHandler.IsInView(worldPos))
+                    _particles[i].position = _transform.InverseTransformPoint(ReflectHandler.GetReflectedPosOf(worldPos));
+            }
+
+            _pSystem.SetParticles(_particles, nParticles);
+        }
+    }
+}
